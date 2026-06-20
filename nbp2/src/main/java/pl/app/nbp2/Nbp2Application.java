cd6f50bd621fc919d754060ad76e896cd7084ca1
TheLis3k <ksawery2k19@gmail.com>
2026-06-20 16:35:13 +0200

    lol

---
+++ nbp2/src/main/java/pl/app/nbp2/Nbp2Application.java
@@ -0,0 +1,13 @@
+package pl.app.nbp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbp2Application.class, args);
+    }
+
+}
---
+++ nbp2/src/test/java/pl/app/nbp2/Nbp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.app.nbp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
